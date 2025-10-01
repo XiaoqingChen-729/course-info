a901c059fcf0f74daa808048eb43a34663b74cc0
XiaoqingChen-729 <chen007@upenn.edu>
2025-10-01 16:26:41 -0400

    Update main.js

--- week05/lecture_examples/data_in_js/js/main.js
+++ week05/lecture_examples/data_in_js/js/main.js
@@ -28,10 +28,33 @@ function gbfsStationToFeature(gbfsStation) {
 window.gbfsStationToFeature = gbfsStationToFeature;
 window.stationsData = stationsData;
 
+// Option1 -const hoodstations = stationsData.data.stations.map(gbfsStationToFeature);
+const hoodstations = [];
+for (const gbfsStation of stationsData.data.stations) {
+  stations.push(gbfsStationToFeature(gbfsStation));
+}
+
 const stations = stationsData.data.stations.map(gbfsStationToFeature);
 
 const stationsLayer = L.geoJSON(stations);
 stationsLayer.addTo(map);
+
+#和下一个for巴拉巴拉的作用是一样的
+
+// Option2
+  const hoodStations = stations.filter(stationInHood);
+  return {
+    type: 'Feature',,
+    id: hood.id,
+    geometry: hood.geometry,
+    properties: {
+      ...hood.properties,
+      stations: hoodStations,
+      stationCount: hoodStations.length,
+    },
+  }
+
+  hoodsCollection.features = hoodsCollection.features.map(enrichHoodFeature);
 
 // Add tooltip with name and bikeshare density...
 for (const hood of hoodsCollection.features) {
@@ -39,7 +62,10 @@ for (const hood of hoodsCollection.features) {
     return turf.booleanPointInPolygon(station, hood);
   }
 
-  const hoodStations = stations.filter(stationInHood);
+  function enrichHoodFeature(hood) {
+    function stationInHood(station) {
+      return turf.booleanPointInPolygon(station, hood);
+    }
 
   const areaSqKm = hood.properties['Shape_Area'] / 3280.84 / 3280.84;
   const stationCount = hoodStations.length;
